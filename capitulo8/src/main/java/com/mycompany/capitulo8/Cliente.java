bec069cff464bbddf8e3a9e55038ced62aece3bc
jjforond <jjforond@bancolombia.com.co>
2024-09-23 12:00:24 -0500

    capitulo8
    Lectura de archivos

---
+++ capitulo8/src/main/java/com/mycompany/capitulo8/Cliente.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.capitulo8;
+
+/**
+ *
+ * @author jjforond
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+     private int numero;
+    private List<String> cuentas;
+
+    public Cliente(int numero) {
+        this.numero = numero;
+        this.cuentas = new ArrayList<>();
+    }
+
+    public void agregarCuenta(String cuenta) {
+        cuentas.add(cuenta);
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public List<String> getCuentas() {
+        return cuentas;
+    }
+    
+}
